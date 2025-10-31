91c4f5f5eba8c254b62a95b13c3f154913a50529
vicdevmanx <vicdevmanx@gmail.com>
2025-10-31 09:49:29 +0100

    fix: fixed & run build for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+  },
 };
 
 export default nextConfig;
