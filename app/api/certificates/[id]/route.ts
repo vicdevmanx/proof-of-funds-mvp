0892e6c7a777caf93f8aea8bce7780c72eb60584
vicdevmanx <vicdevmanx@gmail.com>
2025-11-04 01:14:32 +0100

    feat: fixed deployment error

--- app/api/certificates/[id]/route.ts
+++ app/api/certificates/[id]/route.ts
@@ -4,7 +4,7 @@ import CertificateModel from '@/models/certificate';
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
